4a0dbac23f8e7564885bb0999f539caa6ea9b662
hyun0k <hyunyoung9310@gmail.com>
2021-05-21 15:41:51 +0900

    Add:어드민 회원가입 엔드포인트 구현 - 패스워드 암호화하여 저장 - 셀러 가입하면 셀러 상태를 입점대기로 설정 - account_type_id에 따라 마스터, 셀러 구분하여 저장

--- BackEnd/view/__init__.py
+++ BackEnd/view/__init__.py
@@ -1,8 +1,8 @@
 from view.user_view  import TestView
 from view.order_view import ProductPrepareView 
+from view.seller_view import AccountView
 
 def create_endpoints(app):
     app.add_url_rule('/order/product-prepare', view_func=ProductPrepareView.as_view('product_prepare'))
-######################초기세팅########################
-    app.add_url_rule('/test', view_func=TestView.as_view('test_view'))
-######################초기세팅########################
+    app.add_url_rule('/join/seller', view_func=AccountView.as_view('account_view'))
+

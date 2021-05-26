3732bcb242e68c1a08d95f43900c39d63bfd0076
hyun0k <hyunyoung9310@gmail.com>
2021-05-26 20:27:08 +0900

    Refactor:Import 에러 해결

--- BackEnd/view/__init__.py
+++ BackEnd/view/__init__.py
@@ -1,5 +1,5 @@
-from view.order_view   import ProductPrepareView
-from view.product_view import ProductView
+from view.order_view   import ProductPrepareView, OrderDetailInfoView
+from view.product_view import ProductView, ProductSellerView
 from view.account_view  import SellerAccountView, MasterAccountView, LoginView
 
 def create_endpoints(app):

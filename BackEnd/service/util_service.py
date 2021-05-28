bdb7c408625277c30fb0218ba69a4fc1f5fdd068
ulr0 <ulr0xb@gmail.com>
2021-05-28 15:11:30 +0900

    Modify : 엑셀 파일 생성 시 가상파일로 만들어서 저장되지 않게 수정

--- BackEnd/service/util_service.py
+++ BackEnd/service/util_service.py
@@ -2,39 +2,95 @@ from datetime  import date
 from uuid      import uuid1
 
 from flask import send_file
-
-import xlsxwriter
+# import tempfile
+from io import BytesIO
+from openpyxl.writer.excel import save_virtual_workbook
+from openpyxl import Workbook
+# import xlsxwriter
+# from xlsxwriter import worksheet
 
 class ExcelDownloadService:
     # titles = ['a', 'b', 'c']
     # data = [{}, {}, {}]
+    
     def excel_download(self, titles, data):
+        # today = str(date.today())
+        # uuid  = str(uuid1())
+        
+        # file_name = today + "_" + uuid
+        # print("----------------1----------")
+        # workbook = xlsxwriter.Workbook(
+        #                             f"{file_name}.xlsx",
+        #                             {"tmpdir" : "/Users/ulr0/Downloads"}, 
+        #                             )
+        # print('=======2=============')
+        # worksheet = workbook.add_worksheet()
+
+        # bold = workbook.add_format({'bold': True})
+
+        # row = 0
+        # column = 0
+        # print("===========3================")
+        # for title in titles:
+        #     worksheet.write(row, column, title)
+        #     column += 1
+        
+        # row = 1
+        # print("====================4=================")
+        # for items in data:
+        #     column = 0
+        #     for item in items.values():
+        #         worksheet.write(row, column, item)
+        #         column += 1
+        #     row += 1
+        # print("====================5===============")
+        # workbook.close()
+        # print("==================6==================")
+        
+        
+        
+        
+        
+        # print('==================================')
+        # excel_file = tempfile.NamedTemporaryFile(suffix=".xlsx")
+        # print('=========================1=================')
+        # print(excel_file.name)
+        # workbook = openpyxl.load_workbook(excel_file.name)
+        # print('================3======================')
+        # worksheet = workbook.worksheets[0]
+
+        workbook  = Workbook()
+        worksheet = workbook.active
+
         today = str(date.today())
         uuid  = str(uuid1())
-                
+        
         file_name = today + "_" + uuid
-        
-        workbook = xlsxwriter.Workbook(f"{file_name}.xlsx")
-        worksheet = workbook.add_worksheet()
 
-        bold = workbook.add_format({'bold': True})
-
-        row = 0
-        column = 0
         
-        for title in titles:
-            worksheet.write(row, column, title, bold)
-            column += 1
 
         row = 1
+        column = 1
 
+        print('=============-------------------')
+        for title in titles:
+            worksheet.cell(row, column, title)
+            column += 1
+        
+        row = 2
+        print("====================4=================")
         for items in data:
-            column = 0
+            column = 1
             for item in items.values():
-                worksheet.write(row, column, item)
+                worksheet.cell(row, column, item)
                 column += 1
             row += 1
         
-        workbook.close()
-        
-        return send_file(f"{file_name}.xlsx")
+        excel_file = BytesIO(save_virtual_workbook(workbook))
+
+
+        return send_file(
+        excel_file,
+        attachment_filename=f"{file_name}.xlsx",
+        as_attachment=True
+        )
--- BackEnd/view/__init__.py
+++ BackEnd/view/__init__.py
@@ -1,17 +1,16 @@
-from view.order_view   import ProductPrepareView, OrderDetailInfoView
-from view.product_view import ProductView, ProductSellerView
+from view.order_view    import ProductPrepareView, OrderDetailInfoView
+from view.product_view  import ProductView, ProductSellerView
 from view.account_view  import SellerAccountView, MasterAccountView, LoginView
 from view.master_view  import MasterManageSellerView
 
 def create_endpoints(app):
-    app.add_url_rule('/order/product-prepare', view_func=ProductPrepareView.as_view('product_prepare'))
+    app.add_url_rule("/manage/sellers", view_func=MasterManageSellerView.as_view("master_manage_seller_view"))
+    app.add_url_rule("/manage/sellers/downloads", view_func=MasterManageSellerView.as_view("master_manage_seller_download_view"))
+    app.add_url_rule("/order/product-prepare", view_func=ProductPrepareView.as_view("product_prepare"))
     app.add_url_rule("/join/sellers", view_func=SellerAccountView.as_view("seller_account_view"))
     app.add_url_rule("/join/masters", view_func=MasterAccountView.as_view("master_account_view"))
     app.add_url_rule("/login", view_func=LoginView.as_view("login_view"))
-    app.add_url_rule("/manage/sellers", view_func=MasterManageSellerView.as_view("master_manage_seller_view"))
-    app.add_url_rule("/manage/sellers/downloads", view_func=MasterManageSellerView.as_view("master_manage_seller_download_view"))
-    app.add_url_rule('/order/product-prepare/download', view_func=ProductPrepareView.as_view('excel_download'))
-    app.add_url_rule("/order/order_detail_info", view_func=OrderDetailInfoView.as_view("order_detail_info"))
-
+    app.add_url_rule("/order/product-prepare/download", view_func=ProductPrepareView.as_view("product_prepare_excel_download"))
+    app.add_url_rule("/order/order-detail-info", view_func=OrderDetailInfoView.as_view("order_detail_info"))
     app.add_url_rule("/products", view_func=ProductView.as_view("product_view"))
     app.add_url_rule("/products/seller", view_func=ProductSellerView.as_view("product_seller_view"))

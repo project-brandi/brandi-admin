e50356ab65953e93004b1ba99b30a78179b141d2
ulr0 <ulr0xb@gmail.com>
2021-05-31 21:19:47 +0900

    쿼리문 조건에 주문상태 추가

--- BackEnd/util/const.py
+++ BackEnd/util/const.py
@@ -12,14 +12,13 @@ MASTER_ACCOUNT_TYPE = 1
 SELLER_ACCOUNT_TYPE = 2
 USER_ACCOUNT_TYPE = 3
 
-# shipments_status 배송준비중
-SHIPMENT_STATUS_BEFORE_DELIVERY = 1
+# shipments_status
+SHIPMENT_STATUS_BEFORE_DELIVERY = 1 # 배송준비중
+SHIPMENT_STATUS_SHIPPING = 2 # 배송중
 
-# order_status 주문완료
-ORDER_STATUS_ORDER_COMPLETED = 1
+# order_status 
+ORDER_STATUS_ORDER_COMPLETED = 1 # 주문완료
+ORDER_STATUS_PRODUCT_PREPARE = 2 # 상품준비
+ORDER_STATUS_SHIPPING = 3 # 배송중
 
-# shipments_status, order_status 배송 중
-SHIPPING = 2
 
-# shipments_status, order_status 배송 완료
-DELIVERY_COMPLETED = 3

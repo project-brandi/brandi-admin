eb8a4e8b738fd87254e4d007a9f216fc2d7d4037
ulr0 <ulr0xb@gmail.com>
2021-06-01 09:45:35 +0900

    Modify : 쿼리문 수정 - size, color를 option_info로 합침 - 이름이 모호한 데이터들 alias

--- BackEnd/model/order_detail_dao.py
+++ BackEnd/model/order_detail_dao.py
@@ -4,30 +4,31 @@ class OrderDetailInfoDao:
     def get_order_detail_info(self, connection, data):
         query = """
             SELECT 
-                o.id as order_id,
-                o.created_at as order_created_at,
+                o.id AS order_id,
+                o.created_at AS order_created_at,
                 oh.total_price, 
-                op.id as order_product_id, 
+                op.id AS order_product_id, 
                 os.order_status, 
                 o.paid_at, 
                 oph.order_status_id, 
-                uh.phone_number as order_phone, 
-                p.id, 
-                ph.`name`, 
+                uh.phone_number AS order_phone, 
+                p.id AS product_id, 
+                ph.`name` AS product_name, 
                 oph.price, 
                 oph.discount_rate, 
+                oph.price - truncate(oph.price * (oph.discount_rate / 100), -1) AS sale_price, 
                 sh.korean_name, 
-                sz.size, 
-                c.color, 
+                CONCAT(c.color, "/", sz.size) AS option_info,  
                 oph.quantity, 
-                u.id, 
-                oh.`name` as order_name, 
-                adh.`name`as receive_name, 
-                adh.phone_number as receive_phone, 
+                po.extra_price, 
+                u.id AS user_id, 
+                oh.`name` AS order_name, 
+                adh.`name` AS receive_name, 
+                adh.phone_number AS receive_phone, 
                 adh.address, 
-                smm.content, 
+                smm.content AS shipment_message, 
                 sm.message
-            FROM order_products as op
+            FROM order_products AS op
             INNER JOIN orders AS o 
                 ON op.order_id = o.id
             INNER JOIN order_histories AS oh 
@@ -38,27 +39,27 @@ class OrderDetailInfoDao:
                 ON oph.order_status_id = os.id 
             INNER JOIN users AS u 
                 ON o.user_id = u.id 
-            INNER JOIN user_histories as uh 
+            INNER JOIN user_histories AS uh 
                 ON u.id = uh.user_id 
-            INNER JOIN product_options as po 
+            INNER JOIN product_options AS po 
                 ON op.product_option_id = po.id 
-            INNER JOIN products as p 
+            INNER JOIN products AS p 
                 ON po.product_id = p.id 
-            INNER JOIN product_histories as ph 
+            INNER JOIN product_histories AS ph 
                 ON p.id = ph.product_id 
-            INNER JOIN sellers as s 
+            INNER JOIN sellers AS s 
                 ON p.seller_id = s.id 
-            INNER JOIN seller_histories as sh 
+            INNER JOIN seller_histories AS sh 
                 ON s.id = sh.seller_id 
-            INNER JOIN sizes as sz 
+            INNER JOIN sizes AS sz 
                 ON po.size_id = sz.id 
-            INNER JOIN colors as c 
+            INNER JOIN colors AS c 
                 ON po.color_id = c.id 
-            INNER JOIN shipments as sm 
+            INNER JOIN shipments AS sm 
                 ON op.id = sm.order_product_id 
-            INNER JOIN addresses as ad 
+            INNER JOIN addresses AS ad 
                 ON sm.address_id = ad.id 
-            INNER JOIN address_histories as adh 
+            INNER JOIN address_histories AS adh 
                 ON ad.id = adh.address_id 
             INNER JOIN shipment_memo as smm 
                 ON sm.shipment_memo_id = smm.id 
@@ -75,6 +76,7 @@ class OrderDetailInfoDao:
         query = """
             SELECT 
                 oph.start_time, 
+                oph.end_time,  
                 os.order_status 
             FROM order_product_histories as oph 
             INNER JOIN order_status AS os 
